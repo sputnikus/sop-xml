451261acbca0ea8ac9bbedab7db28e26b28dd564
De.Aleksandra <De.Aleksandra@gmail.com>
2012-05-25 16:33:28 +0000

    @Override

--- src/sop/xml/XmlSemDiff.java
+++ src/sop/xml/XmlSemDiff.java
@@ -39,6 +39,7 @@ public class XmlSemDiff implements XmlSemDiffInterface{
         return content1.equals(content2);
     }
     
+    @Override
     public boolean orderCompare(Element rootEl1, Element rootEl2, boolean 
             similarity) throws NullPointerException{
         
