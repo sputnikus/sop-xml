e89cbb152325595b5c3c5650cfbab20429e7d32c
De.Aleksandra <De.Aleksandra@gmail.com>
2012-05-25 14:44:11 +0000

    public boolean orderCompare(Element rootEl1, Element rootEl2, boolean             similarity) throws NullPointerException{
            if(rootEl1 == null || rootEl2 == null) {
                throw new NullPointerException("Null parameter");
            }
            
            NodeList list1= rootEl1.getChildNodes();
            NodeList list2= rootEl2.getChildNodes();
            int length = list1.getLength();
            
            //comparing name of elements
            for(int n = 0; n < length; n++){
                if (!list1.item(n).getNodeName().equals(list2.item(n).getNodeName())){
                    if(!similarity){
                        return false;
                    }   
                }        
            } 
            return true;
        }

--- src/sop/xml/XmlSemDiff.java
+++ src/sop/xml/XmlSemDiff.java
@@ -37,5 +37,27 @@ public class XmlSemDiff implements XmlSemDiffInterface{
         content2 = content2.replaceAll("\\s", "");
         return content1.equals(content2);
     }
+    
+    public boolean orderCompare(Element rootEl1, Element rootEl2, boolean 
+            similarity) throws NullPointerException{
+        
+        if(rootEl1 == null || rootEl2 == null) {
+            throw new NullPointerException("Null parameter");
+        }
+        
+        NodeList list1= rootEl1.getChildNodes();
+        NodeList list2= rootEl2.getChildNodes();
+        int length = list1.getLength();
+        
+        //comparing name of elements
+        for(int n = 0; n < length; n++){
+            if (!list1.item(n).getNodeName().equals(list2.item(n).getNodeName())){
+                if(!similarity){
+                    return false;
+                }   
+            }        
+        } 
+        return true;
+    }
 
 }
